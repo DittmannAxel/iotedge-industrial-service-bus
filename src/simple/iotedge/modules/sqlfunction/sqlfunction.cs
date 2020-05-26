13c37803752e77967fb69a4b7b77edf1e60c818e
Tilo <tilomlr.dev@gmail.com>
2020-05-26 18:14:03 +0200

    Refactored SqlFunction code

--- src/simple/iotedge/modules/sqlfunction/sqlfunction.cs
+++ src/simple/iotedge/modules/sqlfunction/sqlfunction.cs
@@ -1,149 +1,70 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Azure.Devices.Client;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EdgeHub;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Sql = System.Data.SqlClient;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using DeviceMessage = Microsoft.Azure.Devices.Client.Message;
 
-namespace Functions.Samples
+namespace Industrial.Service.Bus
 {
-    public static class sqlFunction
+    public static class SqlFunction
     {
-        [FunctionName("sqlFunction")]
-        public static async Task FilterMessageAndSendMessage(
-            [EdgeHubTrigger("input1")] Message messageReceived,
-            [EdgeHub(OutputName = "output1")] IAsyncCollector<Message> output,
-            ILogger logger)
+        private static readonly IConfiguration _config;
+
+        static SqlFunction()
         {
-            // const int temperatureThreshold = 20;
-            byte[] messageBytes = messageReceived.GetBytes();
-            var messageString = System.Text.Encoding.UTF8.GetString(messageBytes);
-            
-            logger.LogInformation(messageString);
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+        }
 
-            if (!string.IsNullOrEmpty(messageString))
+        [FunctionName("SqlFunction")]
+        public static async Task StoreMessageAsync([EdgeHubTrigger("input1")] DeviceMessage input, ILogger logger)
+        {
+            var inputMessage = Encoding.UTF8.GetString(input.GetBytes()).Replace(@"\", "");
+
+            if (string.IsNullOrEmpty(inputMessage))
             {
-                logger.LogInformation("Info: Received one non-empty message");
-                // Get the body of the message and deserialize it.                
-                
-                //Remove char in JSON String
-                messageString.Replace(@"\","");
-                
+                logger.LogInformation("Received empty message!");
 
+                return;
+            }
 
-                var messageBody = JsonConvert.DeserializeObject<Envelope>(messageString);
-                var deserializedData = JsonConvert.DeserializeObject<Data>(messageBody.data);
+            logger.LogInformation($"Received message: {inputMessage}");
 
-                //Store the data in SQL db                
-                string str = "Data Source=tcp:sql;Initial Catalog=MeasurementsDB;User ID=SA;Password=Strong!Passw0rd;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;";
-                
-                using (Sql.SqlConnection conn = new Sql.SqlConnection(str))
-                {                    
-                    conn.Open();
-                    
-                    foreach (var item in deserializedData.Contents)
+            var message = JsonConvert.DeserializeObject<Message>(inputMessage);
+
+            var measurementData = new List<string>();
+
+            foreach (var measurement in message.Data.Contents)
+            {
+                foreach (var data in measurement.Data)
+                {
+                    foreach (var value in data.Values)
                     {
-
-                        foreach (var data in item.Data)
-                        {
-
-                            foreach (var value in data.Values)
-                            {
-                                var insertMachinePower = "INSERT INTO dbo.PowerMeasurements VALUES ('"  + messageBody.id + "','" + messageBody.source +"','" + messageBody.type + "','" + messageBody.specversion + "','" + messageBody.datacontenttype + "','" + item.HwId +  "','" + value.Address +  "','" + value.Value +  "','" + data.SourceTimestamp.ToString() + "');";
-                                using (Sql.SqlCommand cmd = new Sql.SqlCommand(insertMachinePower, conn))
-                                {
-                                //Execute the command and log the # rows affected.
-                                    var rows = await cmd.ExecuteNonQueryAsync();
-                                    logger.LogInformation($"{rows} rows were updated");
-                                }
-                            }
-                              
-                        }                      
+                        measurementData.Add($"('{message.Id}','{message.Source}','{message.Type}','{message.SpecVersion}','{message.DataContentType}','{measurement.HwId}','{value.Address}','{value.Value}','{data.SourceTimestamp}')");
                     }
-                    
-                    
-                  
-                    
                 }
+            }
 
-           
+            var insertStatement = $"INSERT INTO dbo.PowerMeasurements VALUES {String.Join(",", measurementData)};";
+
+            using (var connection = new SqlConnection(_config["DatabaseConnection"]))
+            {
+                connection.Open();
+
+                using (var cmd = new SqlCommand(insertStatement, connection))
+                {
+                    var rows = await cmd.ExecuteNonQueryAsync();
+
+                    logger.LogInformation($"{rows} rows were updated");
+                }
+            }
         }
     }
-    }
-   
-    class Envelope {
-
-        [JsonProperty("id")]
-        public string id { get; set; }
-
-        [JsonProperty("source")]
-        public string source { get; set; }
-
-        [JsonProperty("type")]
-        public string type { get; set; }
-
-        [JsonProperty("specversion")]
-        public string specversion { get; set; }
-
-
-        [JsonProperty("datacontenttype")]
-        public string datacontenttype { get; set; }
-
-        [JsonProperty("data")]
-         public string data { get; set; }
-
-    }
-
-    class Data {
-
-        [JsonProperty("PublishTimestamp")]
-        public string PublishTimestamp { get; set; }
-
-        [JsonProperty("Content")]
-        public List<Content> Contents { get; set; }
-
-    }
-
-    class Content {
-
-        [JsonProperty("HwId")]
-        public string HwId { get; set; }
-
-        [JsonProperty("Data")]
-        public List<DataContent> Data { get; set; }
-    }
-
-    class DataContent {
-
-        [JsonProperty("CorrelationId")]
-        public string CorrelationId { get; set; }
-
-        [JsonProperty("SourceTimestamp")]
-        public string SourceTimestamp { get; set; }
-
-        [JsonProperty("Values")]
-        public List<ValueS> Values { get; set; }
-    }
-
-    class ValueS {
-
-        [JsonProperty("Displayname")]
-        public string Displayname { get; set; }
-
-        [JsonProperty("Address")]
-        public string Address { get; set; }
-
-        [JsonProperty("Value")]
-        public string Value { get; set; }
-    }
-
-
 }
-    
-
